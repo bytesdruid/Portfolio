c77d5a0382096804bbd72c9c4757eb25a525af68
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-02-07 14:05:19 -0700

    Updated site info.

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,10 +3,10 @@ const Social = () => {
         <div className="social">
             <h2>Socials</h2>
 
-            <a href="" className="button">john@email.com</a>
-            <a href="https://twitter.com/" className="button">Twitter</a>
-            <a href="https://www.linkedin.com/" className="button">LinkedIn</a>
-            <a href="https://github.com/" className="button">GitHub</a>
+            <a href="" className="button">bloomblockchain@gmail.com</a>
+            <a href="https://twitter.com/AlgoBloom" className="button">Twitter</a>
+            <a href="https://www.linkedin.com/AlgoBloom" className="button">LinkedIn</a>
+            <a href="https://github.com/AlgoBloom" className="button">GitHub</a>
         </div>
     );
 }
