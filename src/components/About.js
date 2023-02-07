4a892b03427fb94ad52a0c90973410b0120e2d6f
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-02-07 14:10:32 -0700

    Added about me.

--- src/components/About.js
+++ src/components/About.js
@@ -6,14 +6,12 @@ const About = () => {
         <section className='about'>
             <h2>About Me</h2>
             <p>
-                Lorem ipsum dolor sit amet consectetur,
-                adipisicing elit. Neque, pariatur odio.
-                Maxime, labore nihil illo ea voluptate suscipit aliquid animi
-                possimus laudantium fugit obcaecati harum?
-                Assumenda maiores nam magnam animi? Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Eaque voluptatibus vitae, rem corrupti id consectetur!
-                Sapiente laboriosam nam excepturi exercitationem natus unde, harum
-                consequatur vero amet consectetur quod sunt autem!
+                Detail oriented, responsible and commited engineer,
+                with a get-it-done, on-time and high-quality product
+                spirit, and more than two years of experience defining
+                requirements, designing, implementing, testing and 
+                delivering complex back-end and web applications using
+                varied programming languages and technologies.
             </p>
         </section>
     );
