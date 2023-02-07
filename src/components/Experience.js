604c634bd9de0de336c6c253ee7de782546ade67
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-02-07 14:17:31 -0700

    Updated Experience.

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -12,17 +12,10 @@ const Experience = () => {
                     </ul>
                 </li>
                 <li>
-                    <i>TechFlow</i> - <strong>Blockchain Developer Intern</strong>, 2018-2019
+                    <i>BlockBeam</i> - <strong>Technical Coordinator</strong>, 2023
                     <ul>
-                        <li>Studied DApp development</li>
-                        <li>Assisted Research &amp; Development on Web3 Products</li>
-                    </ul>
-                </li>
-                <li>
-                    <i>SpiderChain</i> - <strong>Blockchain Developer</strong>, 2019-2022
-                    <ul>
-                        <li>Contributed to smart contract development on client's platform</li>
-                        <li>Peer reviewed &amp; Audited Solidity code</li>
+                        <li>Guide blockchain startup development.</li>
+                        <li>Coordinate with mentors accross the industry.</li>
                     </ul>
                 </li>
             </ul>
