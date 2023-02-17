83ac03a8e5cae9156f91800b888368eb91f948df
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-02-17 06:46:11 -0700

    Corrected LinkedIn username.

--- src/components/Social.js
+++ src/components/Social.js
@@ -5,7 +5,7 @@ const Social = () => {
 
             <a href="" className="button">bloomblockchain@gmail.com</a>
             <a href="https://twitter.com/AlgoBloom" className="button">Twitter</a>
-            <a href="https://www.linkedin.com/AlgoBloom" className="button">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/algobloom/" className="button">LinkedIn</a>
             <a href="https://github.com/AlgoBloom" className="button">GitHub</a>
         </div>
     );
